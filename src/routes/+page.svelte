d925f098defab746279dee977dac0ec9458a8cba
mohan <51280293+paperclouds-src@users.noreply.github.com>
2024-12-19 05:50:34 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<p
+    style="color: #538b72;"
+    >Current Interests</p>
+<p>Komorebi, Trees, Fire Making, Christopher Alexander</p>
+
+<style>
+:global(body) {
+    background-color: #171717;
+    color: #b9b9b9;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 60px 20px;
+}
+
+:global(a) {
+    color: #44797f;
+    text-decoration: none;
+}
+</style>
